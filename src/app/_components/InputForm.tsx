c1ade62f1295d6d1f2e020f3ed7da4e4f76c9b35
Shourya Naik <sjnaik02@gmail.com>
2024-10-11 22:00:42 +0530

    added auto sorting classes + dropdown delete ui

--- src/app/_components/InputForm.tsx
+++ src/app/_components/InputForm.tsx
@@ -1,22 +1,36 @@
-"use client"
+"use client";
 
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { zodResolver } from "@hookform/resolvers/zod"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { useFormStatus } from "react-dom";
 import { useUser } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 
 interface InputFormProps {
   onSubmit: (formData: FormData) => Promise<void>;
 }
 
 const formSchema = z.object({
-  title: z.string().min(1, { message: "Title is required" }).max(100, { message: "Title must be less than 100 characters" }),
-  content: z.string().min(1, { message: "Content is required" }).max(255, { message: "Content must be less than 255 characters" }),
+  title: z
+    .string()
+    .min(1, { message: "Title is required" })
+    .max(100, { message: "Title must be less than 100 characters" }),
+  content: z
+    .string()
+    .min(1, { message: "Content is required" })
+    .max(255, { message: "Content must be less than 255 characters" }),
 });
 
 export const NewInputForm: React.FC<InputFormProps> = ({ onSubmit }) => {
@@ -34,35 +48,60 @@ export const NewInputForm: React.FC<InputFormProps> = ({ onSubmit }) => {
 
   async function onSubmitForm(values: z.infer<typeof formSchema>) {
     const formData = new FormData();
-    formData.append('title', values.title);
-    formData.append('content', values.content);
+    formData.append("title", values.title);
+    formData.append("content", values.content);
     await onSubmit(formData);
     form.reset();
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmitForm)} className="w-full max-w-2xl flex flex-col border py-4 px-2">
-        <FormField control={form.control} name="title" render={({ field }) => (
-          <FormItem>
-            <FormLabel hidden>Title</FormLabel>
-            <FormControl>
-              <Input placeholder="Title" {...field} className="border-none shadow-none placeholder:font-bold placeholder:text-sm" />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )} />
-        <FormField control={form.control} name="content" render={({ field }) => (
-          <FormItem>
-            <FormLabel hidden>Content</FormLabel>
-            <FormControl>
-              <Textarea placeholder="What's on your mind?" {...field} className="border-none shadow-none placeholder:text-base text-base" />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )} />
-        <Button type="submit" disabled={pending || !user} className="self-end rounded-full mt-2 text-base">Post</Button>
+      <form
+        onSubmit={form.handleSubmit(onSubmitForm)}
+        className="flex w-full max-w-2xl flex-col border px-2 py-4"
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel hidden>Title</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Title"
+                  {...field}
+                  className="border-none shadow-none placeholder:text-sm placeholder:font-bold"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel hidden>Content</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="What's on your mind?"
+                  {...field}
+                  className="border-none text-base shadow-none placeholder:text-base"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          disabled={pending || !user}
+          className="mt-2 self-end rounded-full text-base"
+        >
+          Post {pending && <Loader2 className="ml-2 h-4 w-4 animate-spin" />}
+        </Button>
       </form>
     </Form>
-  )
-}
+  );
+};
--- src/app/_components/PostCard.tsx
+++ src/app/_components/PostCard.tsx
@@ -1,7 +1,15 @@
-import { Button } from "@/components/ui/button"
-import { Trash } from "lucide-react"
+import { Button } from "@/components/ui/button";
+import { MoreHorizontal, Trash } from "lucide-react";
 import { deletePost } from "@/server/queries";
 import { auth } from "@clerk/nextjs/server";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 interface PostCardProps {
   id: number;
@@ -12,25 +20,62 @@ interface PostCardProps {
   creatorId: string;
 }
 
-export const PostCard: React.FC<PostCardProps> = ({ id, title, content, createdAt, userName, creatorId }) => {
+export const PostCard: React.FC<PostCardProps> = ({
+  id,
+  title,
+  content,
+  createdAt,
+  userName,
+  creatorId,
+}) => {
   const userId = auth().userId;
 
   return (
-    <div key={id} className="p-4 border rounded-md w-full max-w-2xl relative">
+    <div
+      key={id}
+      className="relative w-full max-w-2xl rounded-md border p-4"
+    >
       <div className="flex items-center text-sm">
-        <h3 className="font-semibold">{title} <span className="mx-2">•</span></h3>
-        <p className=" text-muted-foreground">{userName}</p>
-        <p className="text-sm text-muted-foreground ml-auto">{createdAt.toLocaleDateString()} - {createdAt.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+        <h3 className="font-semibold">
+          {title} <span className="mx-2">•</span>
+        </h3>
+        <p className="text-muted-foreground">{userName}</p>
+        <p className="ml-auto text-sm text-muted-foreground">
+          {createdAt.toLocaleDateString()} -{" "}
+          {createdAt.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
+        </p>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild className="ml-4">
+            <Button variant="ghost" size="icon">
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem asChild>
+              <form
+                action={async () => {
+                  "use server";
+                  await deletePost(id, userId!);
+                }}
+              >
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  type="submit"
+                  disabled={userId !== creatorId}
+                  className="w-full text-red-500"
+                >
+                  Delete
+                </Button>
+              </form>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
       <p className="pt-4 text-lg">{content}</p>
-      {userId === creatorId && (
-        <form action={async () => {
-          "use server"
-          await deletePost(id, userId!);
-        }}>
-          <Button variant="destructive" size="icon" className="mt-2" type="submit"><Trash className="h-4 w-4" /></Button>
-        </form>
-      )}
     </div>
-  )
-}
+  );
+};
